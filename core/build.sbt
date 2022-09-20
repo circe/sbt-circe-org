fa42baa0148be6f0d54cf38ae8b37806e346faa9
Loránd Szakács <lorand.szakacs@protonmail.com>
2022-09-20 20:14:44 +0300

    Remove scalastyle

--- core/build.sbt
+++ core/build.sbt
@@ -2,6 +2,5 @@ val sbtTypelevelVersion = "0.4.14" // https://github.com/typelevel/sbt-typelevel
 
 addSbtPlugin("org.typelevel" % "sbt-typelevel-ci-release" % "0.4.14")
 addSbtPlugin("org.typelevel" % "sbt-typelevel-settings" % "0.4.14")
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0") // https://github.com/scalastyle/scalastyle-sbt-plugin/tags
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.3") // https://github.com/scoverage/sbt-scoverage/releases
 // add everything needed for the website too in some future update
--- core/src/main/scala/io/circe/sbt/CirceIoPlugin.scala
+++ core/src/main/scala/io/circe/sbt/CirceIoPlugin.scala
@@ -1,26 +1,21 @@
 package io.circe.sbt
 
 import scoverage._
-import org.scalastyle.sbt._
 import org.typelevel.sbt._
 import org.typelevel.sbt.gha._
 import sbt.Keys._
 import sbt._
 
 import GenerativeKeys._
-import TypelevelKernelPlugin._
-import autoImport._
 import TypelevelCiPlugin.autoImport._
 import TypelevelSonatypePlugin.autoImport._
-import ScalastylePlugin.autoImport._
-import ScoverageSbtPlugin.autoImport._
 
 object CirceIoPlugin extends AutoPlugin {
   object autoImport
 
   override def trigger = allRequirements
 
-  override def requires = TypelevelCiPlugin && TypelevelSonatypePlugin && ScalastylePlugin && ScoverageSbtPlugin
+  override def requires = TypelevelCiPlugin && TypelevelSonatypePlugin && ScoverageSbtPlugin
 
   override def buildSettings =
     publishSettings ++ organizationSettings ++ githubActionsSettings
@@ -44,19 +39,6 @@ object CirceIoPlugin extends AutoPlugin {
     githubWorkflowJavaVersions := List("11", "17").map(JavaSpec.temurin(_)),
     tlCiScalafixCheck := false,
     githubWorkflowAddedJobs ++= Seq(
-      WorkflowJob(
-        id = "scalafmt",
-        name = "Scalafmt and Scalastyle",
-        scalas = List(crossScalaVersions.value.last),
-        steps = List(WorkflowStep.Checkout) ++ WorkflowStep.SetupJava(
-          List(githubWorkflowJavaVersions.value.last)
-        ) ++ githubWorkflowGeneratedCacheSteps.value ++ List(
-          WorkflowStep.Sbt(
-            List("+scalafmtCheckAll", "scalafmtSbtCheck", "scalastyle"),
-            name = Some("Scalafmt and Scalastyle tests")
-          )
-        )
-      ),
       WorkflowJob(
         id = "coverage",
         name = "Generate coverage report",
