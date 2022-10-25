5113b3771c01b95ab69cdc3df928e90b079a4aba
Loránd Szakács <lorand.szakacs@protonmail.com>
2022-10-25 11:27:55 +0300

    Add scala native auto plugin

--- core/build.sbt
+++ core/build.sbt
@@ -1,7 +1,9 @@
 val sbtTypelevelVersion = "0.4.16" // https://github.com/typelevel/sbt-typelevel/releases
 val sbtScoverageVersion = "2.0.6" // https://github.com/scoverage/sbt-scoverage/releases
+val sbtScalaNativeVersion = "0.4.7" // https://github.com/scala-native/scala-native/releases
 
 addSbtPlugin("org.typelevel" % "sbt-typelevel" % sbtTypelevelVersion)
 addSbtPlugin("org.typelevel" % "sbt-typelevel-site" % sbtTypelevelVersion)
 addSbtPlugin("org.typelevel" % "sbt-typelevel-scalafix" % sbtTypelevelVersion)
+addSbtPlugin("org.scala-native" % "sbt-scala-native" % sbtScalaNativeVersion)
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % sbtScoverageVersion)
---
+++ core/src/main/scala/io/circe/sbt/CirceOrgScalaNativePlugin.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 circe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.circe.sbt
+
+/**
+ * automatically loaded for all projects that are cross-compiled to ScalaJS, no need too reference it explicitly in builds
+ */
+import sbt.*
+import scoverage.ScoverageSbtPlugin.autoImport._
+
+import scala.scalanative.sbtplugin.ScalaNativePlugin
+
+object CirceOrgScalaNativePlugin extends AutoPlugin {
+
+  override def trigger = allRequirements
+
+  override def requires: Plugins = CirceOrgPlugin && ScalaNativePlugin
+
+  override def projectSettings: Seq[Setting[_]] = Seq(
+    coverageEnabled := false
+  )
+
+}
